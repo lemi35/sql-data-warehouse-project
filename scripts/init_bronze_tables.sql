3061595d5e0dd5389152e338150342474cae1466
Lena <83163194+lemi35@users.noreply.github.com>
2026-03-18 00:37:14 +0200

    Initialize bronze schema with new tables
    This script initializes the bronze schema by dropping existing tables and creating new ones for customer, product, sales details, and additional categories.

---
+++ scripts/init_bronze_tables.sql
@@ -0,0 +1,78 @@
+USE DW;
+
+-- make sure schema exists
+SELECT name AS schema_name
+FROM sys.schemas;
+
+
+-- Drop tables if they exist in bronze schema
+
+IF OBJECT_ID('bronze.erp_cust_az12', 'U') IS NOT NULL
+BEGIN
+    DROP TABLE bronze.erp_cust_az12;
+    PRINT 'Dropped bronze.erp_cust_az12';
+END
+
+IF OBJECT_ID('bronze.erp_loc_a101', 'U') IS NOT NULL
+BEGIN
+    DROP TABLE bronze.erp_loc_a101;
+    PRINT 'Dropped bronze.erp_loc_a101';
+END
+
+IF OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
+BEGIN
+    DROP TABLE bronze.erp_px_cat_g1v2;
+    PRINT 'Dropped bronze.erp_px_cat_g1v2';
+END
+
+
+CREATE  TABLE bronze.crm_cust_info (
+	cst_id INT,
+	cst_key NVARCHAR(50),
+	cst_firstname NVARCHAR(50),
+	cst_lastname NVARCHAR(50),
+	cst_marital_status NVARCHAR(50),
+	cst_gndr NVARCHAR(50),
+	cst_create_date DATE
+);
+
+CREATE  TABLE bronze.crm_prd_info (
+	prd_id INT,
+	prd_key NVARCHAR(50),
+	prd_nm NVARCHAR(50),
+	prd_cost INT,
+	prd_line NVARCHAR(50),
+	prd_start_dt DATE,
+	prd_end_dt DATE
+)
+
+CREATE TABLE bronze.crm_sales_details (
+	sls_ord_num NVARCHAR(50),
+	sls_prd_key NVARCHAR(50),
+	sls_cust_id INT,
+	sls_order_dt DATE,
+	sls_ship_dt DATE,
+	sls_due_dt DATE,
+	sls_sales INT,
+	sls_quantity INT,
+	sls_price INT
+)
+
+CREATE TABLE bronze.cust_az12 (
+	CID INT,
+	BDATE DATE,
+	GEN NVARCHAR(50),
+)
+
+CREATE TABLE bronze.loc_a101 (
+	CID INT,
+	CNTRY NVARCHAR(50),
+)
+
+CREATE TABLE bronze.px_cat_g1v2 (
+	ID NVARCHAR(50),
+	CAT NVARCHAR(50),
+	SUBCAT NVARCHAR(50),
+	MAINTENANCE NVARCHAR(50),
+)
+
